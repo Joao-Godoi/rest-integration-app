ef1ba16c151a02077b4f8ae49fc5f876ff18dba9
João Godoi <joaogodoi.dev@gmail.com>
2023-08-22 01:21:12 -0300

    :recycle: refactor: Otimizando e melhorando o código

--- app/src/main/java/com/example/viacepintegration/MainActivity.kt
+++ app/src/main/java/com/example/viacepintegration/MainActivity.kt
@@ -3,10 +3,6 @@ package com.example.viacepintegration
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.example.viacepintegration.databinding.ActivityMainBinding
 import com.example.viacepintegration.http.service.ApiService
@@ -17,7 +13,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,100 +25,70 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.hide()
 
-        findViewById<ImageButton>(R.id.searchCEP).setOnClickListener {
+        binding.searchCEP.setOnClickListener {
             val cep = binding.cep.unMasked
-            if (cep.isEmpty()) {
-                message(it, "Preencha com um CEP válido!")
-            } else if (cep.length != 8) {
-                message(it, "Preencha com um CEP válido!")
+            if (cep.isEmpty() || cep.length != 8) {
+                showSnackBar("Preencha com um CEP válido!")
             } else {
-                val retrofit = Retrofit.Builder().baseUrl("https://viacep.com.br/ws/")
-                    .addConverterFactory(GsonConverterFactory.create()).build()
-                apiService = retrofit.create(ApiService::class.java)
-
-                CoroutineScope(Dispatchers.Main).launch {
-                    val addressInfo = apiService.getViaCepResponse(cep)
-                    setAddressInfo(addressInfo)
-                }
+                setupRetrofit(cep)
             }
         }
 
-        findViewById<Button>(R.id.btnRegister).setOnClickListener {
-
-            val name = findViewById<EditText>(R.id.name).text.toString()
+        binding.btnRegister.setOnClickListener {
+            val name = binding.name.text.toString()
             val bornDate = binding.bornDate.unMasked
-            val email = findViewById<EditText>(R.id.email).text.toString()
+            val email = binding.email.text.toString()
             val phone = binding.phone.unMasked
-            val street = findViewById<EditText>(R.id.street).text.toString()
-            val neighborhood = findViewById<EditText>(R.id.neighborhood).text.toString()
-            val city = findViewById<EditText>(R.id.city).text.toString()
-            val state = findViewById<EditText>(R.id.state).text.toString()
+            val street = binding.street.text.toString()
+            val neighborhood = binding.neighborhood.text.toString()
+            val city = binding.city.text.toString()
+            val state = binding.state.text.toString()
 
             when {
-                name.isEmpty() -> {
-                    message(it, "Preencha seu nome!")
-                }
-
-                bornDate.isEmpty() -> {
-                    message(it, "Preencha sua data de nascimento!")
-                }
-
-                email.isEmpty() -> {
-                    message(it, "Preencha seu email!")
-                }
-
-                !email.contains("@") -> {
-                    message(it, "Email inválido!")
-                }
-
-                phone.isEmpty() -> {
-                    message(it, "Preencha seu telefone!")
-                }
-
-                phone.length < 11 -> {
-                    message(it, "Telefone inválido!")
-                }
-
-                street.isEmpty() -> {
-                    message(it, "Preencha sua rua!")
-                }
-
-                neighborhood.isEmpty() -> {
-                    message(it, "Preencha sua bairro!")
-                }
-
-                city.isEmpty() -> {
-                    message(it, "Preencha sua cidade!")
-                }
-
-                state.isEmpty() -> {
-                    message(it, "Preencha seu estado!")
-                }
-
-                else -> {
-                    navegateToFinish()
-                }
+                name.isEmpty() -> showSnackBar("Preencha seu nome!")
+                bornDate.isEmpty() -> showSnackBar("Preencha sua data de nascimento!")
+                email.isEmpty() -> showSnackBar("Preencha seu email!")
+                !email.contains("@") -> showSnackBar("Email inválido!")
+                phone.isEmpty() || phone.length < 11 -> showSnackBar("Telefone inválido!")
+                street.isEmpty() -> showSnackBar("Preencha sua rua!")
+                neighborhood.isEmpty() -> showSnackBar("Preencha seu bairro!")
+                city.isEmpty() -> showSnackBar("Preencha sua cidade!")
+                state.isEmpty() -> showSnackBar("Preencha seu estado!")
+                else -> navigateToFinish()
             }
         }
     }
 
-    private fun message(view: View, message: String) {
-        val snackbar = Snackbar.make(view, message, Snackbar.LENGTH_SHORT)
-        snackbar.setBackgroundTint(Color.parseColor("#FF0000"))
-        snackbar.setTextColor(Color.parseColor("#FFFFFF"))
-        snackbar.show()
+    private fun showSnackBar(message: String) {
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT).apply {
+            setBackgroundTint(Color.parseColor("#FF0000"))
+            setTextColor(Color.parseColor("#FFFFFF"))
+            show()
+        }
     }
 
-    private fun navegateToFinish() {
-        val intent = Intent(this, Finish::class.java)
-        startActivity(intent)
+    private fun navigateToFinish() {
+        startActivity(Intent(this, Finish::class.java))
+    }
+
+    private fun setupRetrofit(cep: String) {
+        val retrofit = Retrofit.Builder().baseUrl("https://viacep.com.br/ws/")
+            .addConverterFactory(GsonConverterFactory.create()).build()
+        apiService = retrofit.create(ApiService::class.java)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            val addressInfo = apiService.getViaCepResponse(cep)
+            setAddressInfo(addressInfo)
+        }
     }
 
     private fun setAddressInfo(addressInfo: ViaCepResponse) {
-        findViewById<EditText>(R.id.cep).setText(addressInfo.cep)
-        findViewById<EditText>(R.id.street).setText(addressInfo.logradouro)
-        findViewById<EditText>(R.id.neighborhood).setText(addressInfo.bairro)
-        findViewById<EditText>(R.id.city).setText(addressInfo.localidade)
-        findViewById<EditText>(R.id.state).setText(addressInfo.uf)
+        with(binding) {
+            cep.setText(addressInfo.cep)
+            street.setText(addressInfo.logradouro)
+            neighborhood.setText(addressInfo.bairro)
+            city.setText(addressInfo.localidade)
+            state.setText(addressInfo.uf)
+        }
     }
 }
